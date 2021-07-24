f14d6d40c6e1bd4ebf70e7e0944fd50c9e7882c0
veerasaidharreddy-k <sasidhar.karriv@live.com>
2021-07-24 17:09:16 -0400

    Test commit from VS

--- Src/SortingAlgorithms/Program.cs
+++ Src/SortingAlgorithms/Program.cs
@@ -6,7 +6,7 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome Sasidhar to Sorting ALgorithms");
         }
     }
 }
