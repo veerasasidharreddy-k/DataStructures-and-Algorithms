85087b02320c3c816afd87c337574b4cd00b0633
veerasaidharreddy-k <sasidhar.karriv@live.com>
2021-07-26 00:08:22 -0400

    Sorting  - Bubble, Selection and  Insertion sort

--- Src/SortingAlgorithms/Program.cs
+++ Src/SortingAlgorithms/Program.cs
@@ -6,12 +6,16 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            int[] inputArray = new int[10] { 2, 10, 4, 43, 23, 76, 54, 35, 66, 98 };
+            int[] inputArray = new int[10] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 };
             Console.WriteLine("Input Array : {0}", string.Join(", ", inputArray));
 
-            SortingAlgorithms algorithms = new SortingAlgorithms(inputArray);
+            SortingAlgorithms algorithms = new SortingAlgorithms();
 
-            algorithms.BubbleSort();
+            algorithms.BubbleSort(new int[10] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 });
+            algorithms.SelectionSort(new int[10] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 });
+            algorithms.InsertionSort(new int[10] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 });
+            //Best Case
+            algorithms.InsertionSort(new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 });
         }
     }
 }
--- Src/SortingAlgorithms/SortingAlgorithms.cs
+++ Src/SortingAlgorithms/SortingAlgorithms.cs
@@ -3,13 +3,7 @@ namespace SortingAlgorithms
 {
     public class SortingAlgorithms
     {
-        public int[] inputArray;
-        public SortingAlgorithms(int[] _inputArray)
-        {
-            inputArray = _inputArray;
-        }
-
-        public void BubbleSort()
+        public void BubbleSort(int[] inputArray)
         {
             int[] sortedArray = inputArray;
             bool isSwapped;
@@ -23,11 +17,12 @@ namespace SortingAlgorithms
                 {
                     steps++;
                     if (sortedArray[i] > sortedArray[i + 1])
-                    {
-                        int temp = sortedArray[i];
-                        sortedArray[i] = sortedArray[i + 1];
-                        sortedArray[i + 1] = temp;
+                    {//Swapping without extra variable
+                        sortedArray[i] = sortedArray[i] + sortedArray[i + 1];
+                        sortedArray[i + 1] = sortedArray[i] - sortedArray[i + 1];
+                        sortedArray[i] = sortedArray[i] - sortedArray[i + 1];
                         isSwapped = true;
+                        steps++;
                     }
                 }
                 arrayLength--;
@@ -36,10 +31,73 @@ namespace SortingAlgorithms
             PrintOutput("Bubble Sort", sortedArray, steps);
         }
 
+        public void SelectionSort(int[] inputArray)
+        {
+            int[] sortedArray = inputArray;
+            int currStartIndex;
+            int lowestNumberIndex = 0;
+            int steps = 0;
+
+            for(int i = 0; i <= sortedArray.Length-1; i++)
+            {
+                currStartIndex = i;
+                lowestNumberIndex = currStartIndex;
+                for (int j = currStartIndex; j <= sortedArray.Length-1; j++)
+                {
+                    steps++; 
+                    if (sortedArray[j] < sortedArray[lowestNumberIndex])
+                        lowestNumberIndex = j;
+                }
+
+                if(lowestNumberIndex != currStartIndex)
+                {
+                    steps++;
+                    sortedArray[currStartIndex] = sortedArray[currStartIndex] + sortedArray[lowestNumberIndex];
+                    sortedArray[lowestNumberIndex] = sortedArray[currStartIndex] - sortedArray[lowestNumberIndex];
+                    sortedArray[currStartIndex] = sortedArray[currStartIndex] - sortedArray[lowestNumberIndex];
+                }
+                
+            }
+
+            PrintOutput("Selection Sort", sortedArray, steps);
+        }
+
+        public void InsertionSort(int[] inputArray)
+        {
+            int[] sortedArray = inputArray;
+            int steps = 0;
+            int extractedValue;
+            int position;
+            for(int i = 1; i< sortedArray.Length; i++)
+            {
+                steps++;
+                extractedValue = sortedArray[i];
+                position = i - 1;
+
+                do
+                {
+                    steps++;
+                    if (extractedValue < sortedArray[position])
+                    {
+                        steps++;
+                        sortedArray[position + 1] = sortedArray[position];
+                        position--;
+                    }
+                    else
+                        break;
+                } while (position >= 0);
+
+                steps++;
+                sortedArray[position + 1] = extractedValue;
+
+            }
+            PrintOutput("Insertion Sort", sortedArray, steps);
+        }
+
         public void PrintOutput(string algorithmName,int[] sortedArray, int steps = 0)
         {
             Console.WriteLine(algorithmName);
-            Console.WriteLine("Number of loops executed in this algorithm is {0}", steps);
+            Console.WriteLine("Number of significant steps executed in this algorithm is {0}", steps);
 
             Console.WriteLine("Sorted Array : {0}", string.Join(", ", sortedArray));
 
